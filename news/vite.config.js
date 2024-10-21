6dc02c8ea0170995012e113afa2f9566f2ef83f2
Kummirai <ajaxmilton@hotmail.com>
2024-10-21 21:12:46 +0200

    refactor: Update base path in vite.config.js

--- news/vite.config.js
+++ news/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kummirai.github.io/news/",
+  base: "/news/",
   plugins: [react()],
 });
